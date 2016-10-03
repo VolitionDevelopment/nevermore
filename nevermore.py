9a81f2e24555f71504532545dc0bbad15294cc95
Jackson Yeager <jacksonyeager@gmail.com>
2016-10-03 12:13:13 -0400

    Added register and login

--- nevermore.py
+++ nevermore.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flaskext.mysql import MySQL
 import bcrypt
 
@@ -8,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    return redirect('/')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
