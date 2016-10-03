d7cbd6b907c7851b452b2e44d0805d822b4927c2
Jackson Yeager <jacksonyeager@gmail.com>
2016-10-03 10:03:12 -0400

    Added index and layout files

--- nevermore.py
+++ nevermore.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flaskext.mysql import MySQL
 import bcrypt
 
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
